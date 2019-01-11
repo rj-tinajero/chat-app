c36855b2e62259db8fc77387d80819a70c313edb
Richard Tinajero <rj.tinajero92@gmail.com>
2019-01-10 16:42:06 -0800

    assignment 4 done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import RoomList from './components/RoomList';
 import './App.css';
-
+import MessageList from './components/MessageList';
 import * as firebase from 'firebase';
 
 
@@ -16,12 +16,31 @@ var config = {
 firebase.initializeApp(config);
 
 class App extends Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      activeRoom: null
+    };
+    
+    this.setActiveRoom = this.setActiveRoom.bind(this);
+  }
+
+setActiveRoom(room) {
+  this.setState({activeRoom: room})
+}
+
   render() {
     return (
+      <section>
       <div className="App">
         <h1>Chat App</h1>
-        <RoomList firebase= {firebase}/>
+        <RoomList firebase= {firebase} setActiveRoom={this.setActiveRoom} activeRoom={this.state.activeRoom}/>
+        
       </div>
+      <div className="msg-list">
+        <MessageList firebase= {firebase} activeRoom={this.state.activeRoom}/>
+      </div>
+    </section>
     );
   }
 }
---
+++ src/components/MessageList.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+class MessageList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            messages: []
+        };
+
+        this.messagesRef = this.props.firebase.database().ref('messages');
+        
+    }
+
+    componentDidMount() {
+        this.messagesRef.on('child_added', snapshot => {
+            const message = snapshot.val();
+            message.key = snapshot.key;
+            this.setState({ messages: this.state.messages.concat( message )});
+        }); 
+    }
+
+    displayTime() {
+        this.messagesRef.push({
+            sentAt: this.props.firebase.database.ServerValue.TIMPSTAMP
+        });
+    }
+
+    // componentDidUpdate(prevProps){
+    //     if (prevProps.activeRoom !== this.props.activeRoom) {
+    //         console.log("lol", this.props.activeRoom);
+    //         this.setState({messages: []})
+    //         this.messagesRef.orderByChild("roomId").equalTo(this.props.activeRoom.key).on('child_added', snapshot => {
+    //             console.log(snapshot);
+    //             const message = snapshot.val();
+    //             console.log(message);
+    //             message.key = snapshot.key;
+    //             this.setState({ messages: this.state.messages.concat( message )});
+    //         });
+    //     }
+
+    // }
+
+    render() {
+        if (!this.props.activeRoom) {
+            return null;
+        }
+        return (
+            <div>
+                {this.state.messages.filter(message => message.roomId == this.props.activeRoom.key).map((message, index) => (
+                    <div key={index}><span>{message.username}</span><span>{message.content}</span><span>{message.sentAt}</span></div>
+                ))}
+            </div>
+        );
+    }
+}
+
+export default MessageList; 

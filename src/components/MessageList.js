05d1f4f3a82c1c0d956eb5f2727b859239f550da
Richard Tinajero <rj.tinajero92@gmail.com>
2019-01-14 11:22:27 -0800

    last one..

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-/*
+
 class MessageList extends Component {
     constructor(props) {
         super(props);
@@ -8,6 +8,7 @@ class MessageList extends Component {
         };
 
         this.messagesRef = this.props.firebase.database().ref('messages');
+        
     }
 
     componentDidMount() {
@@ -15,13 +16,39 @@ class MessageList extends Component {
             const message = snapshot.val();
             message.key = snapshot.key;
             this.setState({ messages: this.state.messages.concat( message )});
+        }); 
+    }
+
+    displayTime() {
+        this.messagesRef.push({
+            sentAt: this.props.firebase.database.ServerValue.TIMPSTAMP
         });
     }
 
+    // componentDidUpdate(prevProps){
+    //     if (prevProps.activeRoom !== this.props.activeRoom) {
+    //         console.log("lol", this.props.activeRoom);
+    //         this.setState({messages: []})
+    //         this.messagesRef.orderByChild("roomId").equalTo(this.props.activeRoom.key).on('child_added', snapshot => {
+    //             console.log(snapshot);
+    //             const message = snapshot.val();
+    //             console.log(message);
+    //             message.key = snapshot.key;
+    //             this.setState({ messages: this.state.messages.concat( message )});
+    //         });
+    //     }
+
+    // }
+
     render() {
+        if (!this.props.activeRoom) {
+            return null;
+        }
         return (
             <div>
-
+                {this.state.messages.filter(message => message.roomId == this.props.activeRoom.key).map((message, index) => (
+                    <div key={index}><span>{message.username}</span><span>{message.content}</span><span>{message.sentAt}</span></div>
+                ))}
             </div>
         );
     }
